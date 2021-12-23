9b35cbe0332e0fcd9862dd9b7fd694dac5027a2d
Tri Le <trile@users.noreply.github.com>
2021-12-23 20:35:19 +0700

    Test change to UI

--- apps/ui/src/App.js
+++ apps/ui/src/App.js
@@ -38,7 +38,7 @@ class App extends React.Component {
                 <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
                     <a href="/"
                        className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-                        <span className="fs-4">Language Translator</span>
+                        <span className="fs-4">Language Translator Test</span>
                     </a>
                 </header>
 
